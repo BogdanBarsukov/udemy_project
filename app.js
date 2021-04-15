3a70722e2ca5a2e4ff79c8d636ba97d0fb88532f
Bogdan <werque@yandex.ru>
2021-04-15 13:48:55 +0300

    switch & all//

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 "use strict"; //клевая штука - новый стандарт
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?',''), 
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''), 
-      d = prompt('На сколько оцените его?','');
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
-console.log(personalMovieDB);
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms);
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+// const a = prompt('Один из последних просмотренных фильмов?',''), 
+//       b = prompt('На сколько оцените его?',''),
+//       c = prompt('Один из последних просмотренных фильмов?',''), 
+//       d = prompt('На сколько оцените его?','');
+// personalMovieDB.movies[a]=b;//квадратные скобки спасают от косяков
+// personalMovieDB.movies[c]=d;
+// console.log(personalMovieDB);
+
+// const num= 50;
+// switch (num) {
+//   case 49:
+//     console.log('Nope');
+//     break;
+//   case 100:
+//     console.log('Nope');
+//     break;
+//   case 50:
+//     console.log('Yep')
+//     break;
+//   default:
+//     console.log('Nevermind');
+//     break;
+// }// switch === строгое сравнение
