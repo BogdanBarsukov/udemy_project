2d0cf0e267900f8b6df161b7da16baf36f14b59e
Bogdan <werque@yandex.ru>
2021-04-16 15:05:43 +0300

    allToFunctions

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 "use strict"; //клевая штука - новый стандарт
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+start();
 console.log(numberOfFilms);
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,7 +16,8 @@ const personalMovieDB = {
   genres: [],
   privat: false
 };
-for (let i = 0; i < 2; i++) {
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
   const a = prompt('Один из последних просмотренных фильмов?',''),
         b = prompt('На сколько оцените его?','');
   if (a != null && b != null && a.length < 50 && b.length < 50 && a !='' && b != '') {
@@ -18,20 +27,36 @@ for (let i = 0; i < 2; i++) {
     console.log('error');
     i--;
   }
+  }
 }
+rememberMyFilms();
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    alert ('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert ('Вы классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    alert ('Вы киноман');
+  } else {
+    alert ('Произошла ошибка');
+  }  
+}
+detectPersonalLevel();
 console.log(personalMovieDB);
 
-if (personalMovieDB.count < 10) {
-  alert ('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  alert ('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-  alert ('Вы киноман');
-} else {
-  alert ('Произошла ошибка');
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  } 
 }
+showMyDB(personalMovieDB.privat);
 
-
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`);
+  }
+}
+writeYourGenres();
 // const num= 50;
 // switch (num) {
 //   case 49:
