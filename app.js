4f08c1745f772f0b416f17d6954463e42a5ddeed
Bogdan <werque@yandex.ru>
2021-04-16 10:11:47 +0300

    add for/if (home_work)

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 "use strict"; //клевая штука - новый стандарт
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-// console.log(numberOfFilms);
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false
-// };
-// const a = prompt('Один из последних просмотренных фильмов?',''), 
-//       b = prompt('На сколько оцените его?',''),
-//       c = prompt('Один из последних просмотренных фильмов?',''), 
-//       d = prompt('На сколько оцените его?','');
-// personalMovieDB.movies[a]=b;//квадратные скобки спасают от косяков
-// personalMovieDB.movies[c]=d;
-// console.log(personalMovieDB);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?',''),
+        b = prompt('На сколько оцените его?','');
+  if (a != null && b != null && a.length < 50 && b.length < 50 && a !='' && b != '') {
+    personalMovieDB.movies[a]=b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+  alert ('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert ('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  alert ('Вы киноман');
+} else {
+  alert ('Произошла ошибка');
+}
+
 
 // const num= 50;
 // switch (num) {
@@ -31,3 +47,16 @@
 //     console.log('Nevermind');
 //     break;
 // }// switch === строгое сравнение
+// let num = 50;
+// // while (num < 55) {
+// //   console.log(num);
+// //   num++;
+// // }
+// // do {
+// //   console.log(num);
+// //   num++;
+// // } while (num < 55);
+// for (let i = 1; i < 8; i++) {
+//   console.log(num);
+//   num++;
+// }
